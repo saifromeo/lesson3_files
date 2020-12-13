b93bbffffeeca0a265d43eff0c175982fde1ffc2
saifromeo <saifromeo@gmail.com>
2020-12-13 21:34:34 +0530

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -2,7 +2,7 @@ public class hellojava
 {
 	public static void main(String args[]){
 		System.out.println("I am your Java Program. Thank you!");
-		System.out.println("Edited from git");
+		ystem.out.println("Edited from git");
 	}
 }
 
