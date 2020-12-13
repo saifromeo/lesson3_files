b21bcdeb4e4de438c2b49d591606a5290ae07366
Ubuntu <saifromeo@gmail.com>
2020-12-13 15:07:08 +0000

    bug corrected

--- hellojava.java
+++ hellojava.java
@@ -1,7 +1,7 @@
 public class HelloJava
 {
 	public static void main(String args[]){
-		System.out.println("I am your Java Program. Thank you!”);
+		System.out.println("I am your Java Program. Thank you!");
 	}
 }
 
